9e9cecee598d869c66428f2939fb7b641dee96d9
Juliano Ventola <ventolajuliano@gmail.com>
2024-10-31 13:04:35 -0300

    feat: adds player controls and adjustments

---
+++ src/entities.ts
@@ -0,0 +1,199 @@
+import { AreaComp, BodyComp, DoubleJumpComp, GameObj, HealthComp, KaboomCtx, OpacityComp, PosComp, ScaleComp, SpriteComp } from "kaboom";
+import { scale } from "./constants";
+
+type PlayerGameObj = GameObj<
+  SpriteComp &
+  AreaComp &
+  BodyComp &
+  PosComp &
+  ScaleComp &
+  DoubleJumpComp &
+  HealthComp &
+  OpacityComp &
+  {
+    speed: number;
+    direction: string;
+    isInhaling: boolean;
+    isFull: boolean;
+  }
+>
+
+export function makePlayer(k: KaboomCtx, posX: number, posY: number) {
+  const player = k.make([
+    k.sprite("assets", { anim: "kirbIdle" }),
+    k.area({ shape: new k.Rect(k.vec2(4, 5.9), 8, 10) }),
+    k.body(),
+    k.pos(posX * scale, posY * scale),
+    k.scale(scale),
+    k.doubleJump(10),
+    k.health(3),
+    k.opacity(1),
+    {
+      speed: 300,
+      direction: 'right',
+      isInhaling: false,
+      isFull: false,
+    },
+    "player"
+  ])
+
+  player.onCollide("enemy", async (enemy: GameObj) => {
+    if (player.isInhaling && enemy.isInhalable) {
+      player.isInhaling = false;
+      k.destroy(enemy)
+      player.isFull = true
+      return;
+    }
+
+    if (player.hp() === 0) {
+      k.destroy(player)
+      //k.go(globalGameState.currentScene)
+      k.go("level-1")
+      return;
+    }
+
+    player.hurt()
+
+    await k.tween(
+      player.opacity,
+      0,
+      0.05,
+      (val) => (player.opacity = val),
+      k.easings.linear
+    )
+
+    await k.tween(
+      player.opacity,
+      1,
+      0.05,
+      (val) => (player.opacity = val),
+      k.easings.linear
+    )
+  })
+
+  player.onCollide("exit", () => {
+    k.go("level-2")
+  })
+
+  const inhaleEffect = k.add([
+    k.sprite("assets", { anim: "kirbInhalingEffect" }),
+    k.pos(),
+    k.scale(scale),
+    k.opacity(0),
+    "inhaleEffect"
+  ])
+
+  const inhaleZone = player.add([
+    k.area({ shape: new k.Rect(k.vec2(0), 20, 4) }),
+    k.pos(),
+    "inhaleZone"
+  ])
+
+  inhaleZone.onUpdate(() => {
+    if (player.direction === "left") {
+      inhaleZone.pos = k.vec2(-14, 8)
+      inhaleEffect.pos = k.vec2(player.pos.x - 60, player.pos.y + 0)
+      inhaleEffect.flipX = true
+      return
+    }
+    inhaleZone.pos = k.vec2(14, 8)
+    inhaleEffect.pos = k.vec2(player.pos.x + 60, player.pos.y + 0)
+    inhaleEffect.flipX = false
+  })
+
+  player.onUpdate(() => {
+    if (player.pos.y > 2000) {
+      k.go("level-1")
+    }
+  })
+
+  return player
+}
+
+export function setControls(k: KaboomCtx, player: PlayerGameObj) {
+  const inhaleEffectRef = k.get("inhaleEffect")[0]
+  k.onKeyDown((key) => {
+    switch (key) {
+      case "a":
+      case "left":
+        player.direction = "left"
+        player.flipX = true
+        player.move(-player.speed, 0)
+        break;
+
+      case "d":
+      case "right":
+        player.direction = "right"
+        player.flipX = false
+        player.move(player.speed, 0)
+        break;
+
+      case "q":
+      case "z":
+        if (player.isFull) {
+          player.play("kirbFull")
+          inhaleEffectRef.opacity = 0
+          break;
+        }
+
+        player.isInhaling = true
+        player.play("kirbInhaling")
+        inhaleEffectRef.opacity = 1
+        break;
+
+      default:
+        break;
+    }
+  })
+
+  k.onKeyPress((key) => {
+    switch (key) {
+      case "x":
+      case "w":
+        player.doubleJump()
+        break;
+
+      default:
+        break;
+    }
+  })
+
+  k.onKeyRelease((key) => {
+    switch (key) {
+      case "z":
+      case "q":
+        if (player.isFull) {
+          player.play("kirbInhaling")
+          const shootingStar = k.add([
+            k.sprite("assets", {
+              anim: "shootingStar",
+              flipX: player.direction === 'right'
+            }),
+            k.area({ shape: new k.Rect(k.vec2(5, 4), 6, 6) }),
+            k.pos(
+              player.direction === "left" ? player.pos.x - 80 : player.pos.x + 80,
+              player.pos.y + 5
+            ),
+            k.scale(scale),
+            player.direction === "left" ?
+              k.move(k.LEFT, 800) :
+              k.move(k.RIGHT, 800),
+            "shootingStar"
+          ])
+          shootingStar.onCollide("platform", () => k.destroy(shootingStar))
+
+          player.isFull = false
+          k.wait(1, () => player.play("kirbIdle"))
+          return
+        }
+
+        inhaleEffectRef.opacity = 0
+        player.isInhaling = false
+        player.play("kirbIdle")
+        break;
+
+      default:
+        break;
+    }
+  })
+}
---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import { makePlayer, setControls } from "./entities";
+import { k } from "./kaboomCtx";
+import { makeMap } from "./utils";
+
+async function gameSetup() {
+  k.loadSprite("assets", "./kirby-like.png", {
+    sliceX: 9,
+    sliceY: 10,
+    anims: {
+      kirbIdle: 0,
+      kirbInhaling: 1,
+      kirbFull: 2,
+      kirbInhalingEffect: { from: 3, to: 8, speed: 15, loop: true },
+      shootingStar: 9,
+      flame: { from: 36, to: 37, speed: 4, loop: true },
+      guyIdle: 18,
+      guyWalk: { from: 18, to: 19, speed: 4, loop: true },
+      bird: { from: 27, to: 28, speed: 4, loop: true },
+    }
+  })
+
+  k.loadSprite("level-1", "./level-1.png");
+
+  const { map: level1Layout, spawnPoints: level1SpawnPoints } = await makeMap(k, "level-1")
+
+  k.scene("level-1", () => {
+    k.setGravity(2100);
+    k.add([
+      k.rect(k.width(), k.height()),
+      k.color(k.Color.fromHex('#f7d7db')),
+      k.fixed(),
+    ])
+    k.add(level1Layout)
+
+    const kirb = makePlayer(k,
+      level1SpawnPoints.player[0].x,
+      level1SpawnPoints.player[0].y
+    )
+
+    setControls(k, kirb)
+
+    k.add(kirb)
+    k.camScale(0.7, 0.7)
+    k.onUpdate(() => {
+      if (kirb.pos.x < level1Layout.pos.x + 432) {
+        k.camPos(kirb.pos.x + 500, 870)
+      }
+    })
+  })
+
+  k.go("level-1")
+
+
+}
+
+gameSetup() 
